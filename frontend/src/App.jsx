ba2b6ae0573b6527278cbc74a096ebbebf614d5a
labdinouha-png <labdi.nouha@univ-constantine2.dz>
2026-05-06 20:55:53 +0100

    final changes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -71,7 +71,7 @@ const ProtectedRoute = ({ children, allowedRoles, allowedSubRoles }) => {
 function App() {
   // Ping backend on app load to wake it up
 useEffect(() => {
-  fetch('https://pfe-l31r.onrender.com/api/auth/check-user/')
+  fetch('https://pfe-l31r.onrender.com')
     .catch(() => {}); // ignore errors, just wake it up
 }, []);
   return (
