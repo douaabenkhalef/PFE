02564bd1ce965917e82099b87d7b4cf681be6084
douaabenkhalef <douaa.benkhalef@univ-constantine2.dz>
2026-05-11 22:24:28 +0100

    Update websocket URL patterns in asgi.py

--- backend/backend/asgi.py
+++ backend/backend/asgi.py
@@ -1,4 +1,4 @@
-# backend/asgi.py
+
 import os
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
@@ -9,16 +9,16 @@ from api import consumers
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
 
 websocket_urlpatterns = [
-    # Chat université (pour les admins)
+    
     re_path(r'ws/chat/(?P<university_name>[^/]+)/$', consumers.ChatConsumer.as_asgi()),
     
-    # 🔥 Chat entreprise - utiliser company_name comme paramètre
+    
     re_path(r'ws/company-chat/(?P<company_name>[^/]+)/$', consumers.CompanyGroupChatConsumer.as_asgi()),
     
-    # Chat par stage (pour les étudiants)
+    
     re_path(r'ws/internship-chat/(?P<internship_id>[^/]+)/$', consumers.InternshipGroupChatConsumer.as_asgi()),
     
-    # Chat privé
+ 
     re_path(r'ws/private-chat/$', consumers.PrivateChatConsumer.as_asgi()),
 ]
 
